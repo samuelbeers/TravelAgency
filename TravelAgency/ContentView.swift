0ee525206e1ab487338ba29f82df62113c50560a
qoietsammy <real.samuel.beers@gmail.com>
2024-11-18 10:20:17 -0500

    Initial commit

---
+++ TravelAgency/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TravelAgency
+//
+//  Created by Sam Beers on 11/18/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TravelAgency/TravelAgencyApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelAgencyApp.swift
+//  TravelAgency
+//
+//  Created by Sam Beers on 11/18/24.
+//
+
+import SwiftUI
+
+@main
+struct TravelAgencyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
